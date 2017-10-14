a3542a69fa9ee61ed7b8916480d2c92359bdeee4
David Muñoz <davidmzm@gmail.com>
2017-10-14 11:47:58 -0500

    Created functions to calculate time and date in the right components.

--- src/scenes/Landing/components/DateTime/date.js
+++ src/scenes/Landing/components/DateTime/date.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import './date.css';
 
-class Date extends Component {
+class DateComponent extends Component {
   render() {
     return (
         <div className="date">
-            02 october 2017
+            {getDate()}
         </div>
     );
   }
 }
 
-export default Date
+function getDate() {
+  let today = new Date();
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let dd = today.getDate();
+  let mm = today.getMonth();
+  let yyyy = today.getFullYear();
+
+  return dd + " " + months[mm] + " " + yyyy;
+}
+
+export default DateComponent
--- src/scenes/Landing/components/DateTime/time.js
+++ src/scenes/Landing/components/DateTime/time.js
@@ -5,10 +5,28 @@ class Time extends Component {
   render() {
     return (
         <div className="time">
-            09:57 p.m.
+            {getTime()}
         </div>
     );
   }
+}
+
+function getTime() {
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let ampm;
+  
+  if (hours > 12) {
+    ampm = "p.m.";
+    hours -= hours;
+  } else {
+    ampm = "a.m.";
+  }
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  return hours + ":" + minutes + " " + ampm;
 }
 
 export default Time
