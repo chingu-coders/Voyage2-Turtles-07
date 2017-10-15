32228bec0712f31f0f0e0b2076f501650d088ee2
David Muñoz <davidmzm@gmail.com>
2017-10-15 18:02:05 -0500

    Fixed time to update every 60 seconds.

--- src/scenes/Landing/components/DateTime/time.js
+++ src/scenes/Landing/components/DateTime/time.js
@@ -2,27 +2,55 @@ import React, { Component } from 'react';
 import './time.css';
 
 class Time extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeFormat: true,
+      time: getTime(true)
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      60000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      time: getTime(this.state.timeFormat)
+    });
+  }
+  
   render() {
     return (
         <div className="time">
-            {getTime()}
+            {this.state.time}
         </div>
     );
   }
 }
 
+
 function getTime() {
   let today = new Date();
   let hours = today.getHours();
   let minutes = today.getMinutes();
-  let ampm;
+  let ampm = "";
   
-  if (hours > 12) {
+  if (hours >= 12) {
     ampm = "p.m.";
-    hours -= hours;
+    hours = hours - 12;
+    hours = hours ? hours : 12;
   } else {
     ampm = "a.m.";
   }
+
   hours = hours < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
 
